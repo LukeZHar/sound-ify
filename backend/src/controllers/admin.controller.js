f9d26841883769464d6710ad25374634b9666a0f
LukeZHar <luke.z.harris@hotmail.com>
2025-11-23 15:37:40 +1000

    feat: add admin and user controllers, and update routes for authentication and admin access

---
+++ backend/src/controllers/admin.controller.js
@@ -0,0 +1,4 @@
+export const getAdmin = (req, res) => {
+    // Logic to retrieve admin details from the database
+    res.send('Retrieve admin details');
+}
---
+++ backend/src/controllers/user.controller.js
@@ -0,0 +1,4 @@
+const getAllUsers = (req, res) => {
+    // Logic to retrieve all users from the database
+    res.send('Retrieve all users');
+}
