074c95d52bb92a8229dc7550927a23dc81fb60e3
LukeZHar <luke.z.harris@hotmail.com>
2025-11-24 14:15:41 +1000

    feat: implement song controller and routes for fetching all songs, featured songs, made for you songs, and trending songs

--- backend/src/routes/song.route.js
+++ backend/src/routes/song.route.js
@@ -1,9 +1,17 @@
 import { Router } from "express";
+import { protectRoutes, requireAdmin } from "../middleware/auth.middleware.js";
+import {
+  getAllSongs,
+  getFeaturedSongs,
+  getMadeForYouSongs,
+  getTrendingSongs,
+} from "../controllers/song.controller.js";
 
 const router = Router();
 
-router.get("/", (req, res) => {
-  res.send("Songs route is working!");
-});
+router.get("/", protectRoutes, requireAdmin, getAllSongs);
+router.get("/featured", getFeaturedSongs);
+router.get("/made-for-you", getMadeForYouSongs);
+router.get("/trending", getTrendingSongs);
 
 export default router;
