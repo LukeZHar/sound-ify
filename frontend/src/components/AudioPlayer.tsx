e36d323ec4ecf13125fcf5ffdbc8f54f91f41046
LukeZHar <luke.z.harris@hotmail.com>
2025-11-30 18:18:25 +1000

    feat: implement AudioPlayer component with playback controls and song change handling

--- frontend/src/components/AudioPlayer.tsx
+++ frontend/src/components/AudioPlayer.tsx
@@ -1,9 +1,33 @@
-
+import { usePlayerStore } from "@/stores/usePlayerStore";
+import { useEffect, useRef } from "react";
 
 const AudioPlayer = () => {
-  return (
-    <div>AudioPlayer</div>
-  )
-}
+  const audioRef = useRef<HTMLAudioElement>(null);
+  const prevSongRef = useRef<string | null>(null);
+  const { currentSong, isPlaying, playNext } = usePlayerStore();
+  // Handle play and pause logic
+  useEffect(() => {
+    if (isPlaying) {
+      audioRef.current?.play();
+    } else {
+      audioRef.current?.pause();
+    }
+  }, [isPlaying]);
 
-export default AudioPlayer
+  // Handle song change logic
+  useEffect(() => {
+    const audio = audioRef.current;
+    const handleEnded = () => {
+      playNext();
+    };
+    audio?.addEventListener("ended", handleEnded);
+
+    return () => {
+      audio?.removeEventListener("ended", handleEnded);
+    };
+  }, [currentSong, playNext]);
+  
+  return <audio ref={audioRef} />;
+};
+
+export default AudioPlayer;
