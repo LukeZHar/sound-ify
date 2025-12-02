b11e7c261b0ed7ded7c5e823815f33ce54dc6e23
LukeZHar <luke.z.harris@hotmail.com>
2025-12-02 16:03:13 +1000

    feat: add PlayButton component for song playback control in FeaturedSection

--- frontend/src/components/FeaturedSection.tsx
+++ frontend/src/components/FeaturedSection.tsx
@@ -1,5 +1,6 @@
 import { useMusicStore } from "@/stores/useMusicStore";
 import FeaturedGridSkeleton from "./skeletons/FeaturedGridSkeleton";
+import PlayButton from "./PlayButton";
 
 const FeaturedSection = () => {
   const { isLoading, featuredSongs, error } = useMusicStore();
@@ -23,8 +24,9 @@ const FeaturedSection = () => {
             <p className="font-medium truncate">{song.title}</p>
             <p className="text-sm text-zinc-400 truncate">{song.artist}</p>
           </div>
+          {/* TODO: add play button */}
+          <PlayButton song={song} />
         </div>
-        // TODO: add play button
       ))}
     </div>
   );
