c1621583a63545e1d97c078fd18a2154f241de2d
LukeZHar <luke.z.harris@hotmail.com>
2025-12-14 16:03:41 +1000

    feat: enhance socket integration with improved message handling and user activity tracking

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -1,29 +1,72 @@
-import {Server} from "socket.io";
-import {Message} from "../models/Message.model.js";
+import { Server } from "socket.io";
+import { Message } from "../models/Message.model.js";
 
 export const initializeSocket = (server) => {
-    const io = new Server(server, {
-        cors: {
-            origin: "http://localhost:3000",
-            credentials: true,
-        }
-    })
+  const io = new Server(server, {
+    cors: {
+      origin: "http://localhost:3000",
+      credentials: true,
+    },
+  });
 
-    const userSocket = new Map(); // Map to store userId to socketId mapping
-    const userActivities = new Map(); // Map to store userId to activity status
+  const userSocket = new Map(); // Map to store userId to socketId mapping
+  const userActivities = new Map(); // Map to store userId to activity status
 
-    io.on("connection", (socket) => {
-        socket.on("user_connected", (userId) => {
-            userSocket.set(userId, socket.id);
-            userActivities.set(userId, "idle");
+  io.on("connection", (socket) => {
+    socket.on("user_connected", (userId) => {
+      userSocket.set(userId, socket.id);
+      userActivities.set(userId, "idle");
 
-            // Notify other users about the new connection
-            io.emit("user_connected", userId);
+      // Notify other users about the new connection
+      io.emit("user_connected", userId);
 
-            socket.emit("users_online", Array.from(userSocket.keys()));
+      socket.emit("users_online", Array.from(userSocket.keys()));
 
-            // Send current activities to the newly connected user
-            io.emit("activities", Array.from(userActivities.entries()));
-        })
+      // Send current activities to the newly connected user
+      io.emit("activities", Array.from(userActivities.entries()));
     });
-}
+
+    socket.on("update_activity", ({ userId, activity }) => {
+      console.log(`User ${userId} updated activity to ${activity}`);
+      userActivities.set(userId, activity);
+      io.emit("activity_updated", { userId, activity });
+    });
+
+    socket.on("send_message", async (data) => {
+      try {
+        const { senderId, receiverId, content } = data;
+
+        const message = await Message.create({
+          senderId,
+          receiverId,
+          content,
+        });
+
+        // send to receiver in realtime if online
+        const receiverSocketId = userSocket.get(receiverId);
+        if (receiverSocketId) {
+          io.to(receiverSocketId).emit("receive_message", message);
+        }
+        socket.emit("message_sent", message);
+      } catch (error) {
+        console.error("Error sending message:", error);
+        socket.emit("Failed to send message", error.message);
+      }
+    });
+
+    socket.on("disconnect", () => {
+      let disconnectedUserId;
+      for (const [userId, socketId] of userSocket.entries()) {
+        // find the disconnected userId
+        if (socketId === socket.id) {
+          userSocket.delete(userId);
+          userActivities.delete(userId);
+          break;
+        }
+      }
+      if (disconnectedUserId) {
+        io.emit("user_disconnected", disconnectedUserId);
+      }
+    });
+  });
+};
