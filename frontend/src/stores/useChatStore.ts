05ac4326fb828bef37a7f9c97dd970f1efc1ada3
LukeZHar <luke.z.harris@hotmail.com>
2025-12-14 17:34:06 +1000

    feat: implement socket connection and message handling in chat store

--- frontend/src/stores/useChatStore.ts
+++ frontend/src/stores/useChatStore.ts
@@ -1,17 +1,39 @@
 import { axiosInstance } from "@/lib/axios";
+import type { Message } from "@/types";
 import { create } from "zustand";
+import { io } from "socket.io-client";
 
 interface ChatStore {
   users: any[];
-  fetchUsers: () => Promise<void>;
   isLoading: boolean;
   error: string | null;
+  socket: any;
+  isConnected: boolean;
+  onlineUsers: Set<string>;
+  userActivities: Map<string, string>;
+  messages: Message[];
+  fetchUsers: () => Promise<void>;
+  initSocket: (userId: string) => void;
+  disconnectSocket: () => void;
+  sendMessage: (receiverId: string, senderId: string, content: string) => void;
 }
 
-export const useChatStore = create<ChatStore>((set) => ({
+const baseUrl = "http://localhost:5000";
+
+const socket = io(baseUrl, {
+  autoConnect: false, // Only connect when user is authenticated
+  withCredentials: true,
+});
+
+export const useChatStore = create<ChatStore>((set, get) => ({
   users: [],
   isLoading: false,
   error: null,
+  socket: null,
+  isConnected: false,
+  onlineUsers: new Set(),
+  userActivities: new Map(),
+  messages: [],
 
   fetchUsers: async () => {
     set({ isLoading: true, error: null });
@@ -24,4 +46,47 @@ export const useChatStore = create<ChatStore>((set) => ({
       set({ isLoading: false });
     }
   },
+
+  initSocket: (userId: string) => {
+    if (!get().isConnected) {
+      socket.connect();
+      socket.emit("user_connected", userId);
+
+      socket.on("user_online", (user: string[]) => {
+        set({ onlineUsers: new Set(user) });
+      });
+
+      socket.on("activities", (activities: [string, string][]) => {
+        set({ userActivities: new Map(activities) });
+      });
+
+      socket.on("user_connected", (userId: string) => {
+        set((state) => ({
+          onlineUsers: new Set([...state.onlineUsers, userId]),
+        }));
+      });
+
+      socket.on("user_disconnected", (userId: string) => {
+        set((state) => {
+          const newOnlineUsers = new Set(state.onlineUsers);
+          newOnlineUsers.delete(userId);
+          return { onlineUsers: newOnlineUsers };
+        });
+      });
+
+      socket.on("receive_message", (message: Message) => {
+        set((state) => ({
+          messages: [...state.messages, message],
+        }));
+      });
+
+      socket.on("message_sent", (message: Message) => {
+        set((state) => ({
+          messages: [...state.messages, message],
+        }));
+      });
+    }
+  },
+  disconnectSocket: () => {},
+  sendMessage: async () => {},
 }));
