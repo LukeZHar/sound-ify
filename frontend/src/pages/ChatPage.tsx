a602f310affa3b815cda6213bfe6434cb802d706
LukeZHar <luke.z.harris@hotmail.com>
2025-12-16 18:27:05 +1000

    feat: add ChatHeader component to display selected user information in ChatPage

--- frontend/src/pages/ChatPage.tsx
+++ frontend/src/pages/ChatPage.tsx
@@ -1,3 +1,4 @@
+import ChatHeader from "@/components/ChatHeader";
 import Topbar from "@/components/Topbar";
 import UserList from "@/components/UserList";
 import { useChatStore } from "@/stores/useChatStore";
@@ -21,9 +22,39 @@ const ChatPage = () => {
       <Topbar />
       <div className="grid lg:grid-cols-[300px_1fr] grid-cols-[80px_1fr] h-[calc(100vh-180px)]">
         <UserList />
+        {/* Chat Area */}
+        <div className="flex flex-col h-full">
+          {selectedUser ? (
+            <>
+              <ChatHeader />
+            </>
+          ) : (
+            <NoConversationSelected />
+          )}
+        </div>
       </div>
     </main>
   );
 };
 
 export default ChatPage;
+
+const NoConversationSelected = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-full space-y-6">
+      <img
+        src="/spotify.png"
+        alt="Soundify"
+        className="size-16 animate-bounce"
+      />
+      <div className="text-center">
+        <h3 className="text-zinc-300 text-lg font-medium mb-1">
+          No Conversation Selected
+        </h3>
+        <p className="text-zinc-500 text-sm">
+          Choose a conversation to start chatting.
+        </p>
+      </div>
+    </div>
+  );
+};
